78d6b06854857293ad738cdd4bf8e46a601d2418
TamNhien <canhthien91@gmail.com>
2024-03-27 19:17:11 +0700

    Add project files.

---
+++ P_InPhanTuDuongDauTien.py
@@ -0,0 +1,10 @@
+﻿def find_first_positive(lst):
+    return next((num for num in lst if num > 0), -1)
+
+def main():
+    lst = list(map(int, input("Nhập vào danh sách các số nguyên, cách nhau bởi dấu phẩy: ").split(',')))
+    print(find_first_positive(lst))
+
+if __name__ == "__main__":
+    main()
+
